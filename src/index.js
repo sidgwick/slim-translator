7a73fc6fd6d8fee36b9a168e5a05fa89e353dacb
Zhigang Song <song@zgang.me>
2018-04-06 19:43:52 +0800

    hidden translate result

--- src/index.js
+++ src/index.js
@@ -11,29 +11,80 @@ import TranslateResult from './components/TranslateResult';
 
 var translator = {
   root_ele_id: "__slim_translate_root",
-  tooltip_ele_id: "__slim_translate_button",
+  button_ele_id: "__slim_translate_button",
   result_ele_id: "__slim_translate_result_area",
   event: null,
   text: null,
 
   run: function (event) {
-    let selection = document.getSelection();
-
     this.event = event;
+    this.initTranslateRootArea();
+
+    if (document.getElementById(this.button_ele_id)) {
+      // 走到这里表示在:
+      // 1. 点击翻译按钮, 由按钮 onClick 来处理接下来的动作
+      // 2. 点击翻译按钮以外的地方, 清空翻译根 DOM
+      this.tryUmountTranslateButton();
+      return;
+    }
+
+    if (document.getElementById(this.result_ele_id)) {
+      // 走到这里判断是不是点击在了显示结果区域
+      // 1. 是, 不做任何处理
+      // 2. 不是, 清空翻译根 DOM
+      this.tryUmountTranslateResult();
+      return;
+    }
+
+    let selection = document.getSelection();
     this.text = selection.toString();
     if (!this.text) {
       return;
     }
 
-    let trans_root = document.getElementById(this.root_ele_id);
-    if (!trans_root) {
-      let body = document.getElementsByTagName("body")[0];
-      let root_div = document.createElement("div");
-      root_div.id = this.root_ele_id;
-      body.appendChild(root_div);
+    this.display_tooltip();
+  },
+
+  tryUmountTranslateButton: function () {
+    let button = document.getElementById(this.button_ele_id);
+    this.tryClearTranslateRootDom(button);
+  },
+
+  tryUmountTranslateResult: function () {
+    let result = document.getElementById(this.result_ele_id);
+    this.tryClearTranslateRootDom(result);
+  },
+
+  tryClearTranslateRootDom: function (content) {
+    let pos = content.getBoundingClientRect();
+
+    let x = this.event.clientX;
+    let y = this.event.clientY;
+
+    if (pos.x <= x
+        && x <= (pos.x + pos.width)
+        && pos.y <= y
+        && y <= (pos.y + pos.height)
+    ) {
+      // 点击到了按钮上
+      return;
     }
 
-    this.display_tooltip();
+    // 清空翻译根 DOM
+    let root = document.getElementById(this.root_ele_id);
+    ReactDOM.unmountComponentAtNode(root);
+  },
+
+  initTranslateRootArea: function () {
+    let trans_root = document.getElementById(this.root_ele_id);
+    if (trans_root) {
+      return;
+    }
+
+    let body = document.getElementsByTagName("body")[0];
+    let root_div = document.createElement("div");
+    root_div.id = this.root_ele_id;
+    body.appendChild(root_div);
   },
 
   tooltip_position: function () {
@@ -46,12 +97,16 @@ var translator = {
   display_tooltip: function () {
     let pos = this.tooltip_position();
     let root_ele = document.getElementById(this.root_ele_id);
-    ReactDOM.render(<TranslateButton id={this.tooltip_ele_id} pos={pos} onClick={() => this.translate()} />, root_ele);
+    ReactDOM.render(<TranslateButton id={this.button_ele_id} pos={pos} onClick={() => this.translate()} />, root_ele);
+  },
+
+  display_translate_result: function (res) {
+    let pos = this.tooltip_position();
+    let root_ele = document.getElementById(this.root_ele_id);
+    ReactDOM.render(<TranslateResult id={this.result_ele_id} pos={pos} res={res} />, root_ele);
   },
 
   translate: function () {
-    console.log("starting translate");
-
     gt(this.text, {
       to: 'zh-CN'
     }).then(res => {
@@ -62,12 +117,6 @@ var translator = {
       console.log("google is dead");
       console.error(err);
     });
-  },
-
-  display_translate_result: function (res) {
-    let pos = this.tooltip_position();
-    let root_ele = document.getElementById(this.root_ele_id);
-    ReactDOM.render(<TranslateResult id={this.result_ele_id} pos={pos} res={res} />, root_ele);
   },
 };
 
